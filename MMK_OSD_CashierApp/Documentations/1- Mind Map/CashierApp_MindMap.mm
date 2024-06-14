785f5b895cd4db9c129504457d19e8b94d329f74
Mehrbod Molla Kazemi <98255802+Mehrbod-MK@users.noreply.github.com>
2024-06-14 10:25:30 +0330

    Created Mind map.

---
+++ MMK_OSD_CashierApp/Documentations/1- Mind Map/CashierApp_MindMap.mm
@@ -0,0 +1,75 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1718346583267" ID="ID_1054900639" MODIFIED="1718346805504" TEXT="&#x645;&#x62f;&#x6cc;&#x631;&#x6cc;&#x62a; &#x635;&#x646;&#x62f;&#x648;&#x642; &#x641;&#x631;&#x648;&#x634;&#x6af;&#x627;&#x647; &#x632;&#x646;&#x62c;&#x6cc;&#x631;&#x647;&#x200c;&#x627;&#x6cc;">
+<font NAME="SansSerif" SIZE="20"/>
+<icon BUILTIN="idea"/>
+<node CREATED="1718346711406" ID="ID_216447295" MODIFIED="1718346989595" POSITION="right" TEXT="&#x645;&#x634;&#x62a;&#x631;&#x6cc;" VSHIFT="-49">
+<font NAME="SansSerif" SIZE="20"/>
+<icon BUILTIN="bookmark"/>
+<icon BUILTIN="male1"/>
+<node CREATED="1718346991961" HGAP="29" ID="ID_1674893140" MODIFIED="1718347061867" TEXT="&#x62d;&#x633;&#x627;&#x628; &#x6a9;&#x627;&#x631;&#x628;&#x631;&#x6cc;" VSHIFT="-80">
+<font NAME="SansSerif" SIZE="16"/>
+<node CREATED="1718347032728" HGAP="31" ID="ID_1246506702" MODIFIED="1718347052423" TEXT="&#x646;&#x627;&#x645;" VSHIFT="-24"/>
+<node CREATED="1718347038688" HGAP="37" ID="ID_820651850" MODIFIED="1718347050473" TEXT="&#x646;&#x627;&#x645; &#x62e;&#x627;&#x646;&#x648;&#x627;&#x62f;&#x6af;&#x6cc;" VSHIFT="-54"/>
+<node CREATED="1718347065208" HGAP="48" ID="ID_1745903663" MODIFIED="1718347121261" TEXT="&#x6a9;&#x62f; &#x645;&#x644;&#x6cc;" VSHIFT="-35"/>
+<node CREATED="1718347068592" HGAP="49" ID="ID_821909481" MODIFIED="1718347077571" TEXT="&#x634;&#x645;&#x627;&#x631;&#x647; &#x62a;&#x645;&#x627;&#x633;" VSHIFT="-1"/>
+</node>
+<node CREATED="1718347171160" ID="ID_1877467072" MODIFIED="1718347367073" TEXT="&#x62e;&#x631;&#x6cc;&#x62f; &#x645;&#x62d;&#x635;&#x648;&#x644;&#x627;&#x62a; &#x645;&#x648;&#x631;&#x62f; &#x646;&#x638;&#x631;">
+<font NAME="SansSerif" SIZE="17"/>
+<node CREATED="1718347180369" ID="ID_1294394680" MODIFIED="1718347226796" TEXT="&#x645;&#x62f;&#x6cc;&#x631;&#x6cc;&#x62a; &#x633;&#x628;&#x62f; &#x62e;&#x631;&#x6cc;&#x62f; &#x645;&#x634;&#x62a;&#x631;&#x6cc;" VSHIFT="-3"/>
+</node>
+<node CREATED="1718347353681" ID="ID_1352579699" MODIFIED="1718347360961" TEXT="&#x645;&#x631;&#x627;&#x62c;&#x639;&#x647; &#x628;&#x647; &#x635;&#x646;&#x62f;&#x648;&#x642;&#x62f;&#x627;&#x631;">
+<font NAME="SansSerif" SIZE="15"/>
+</node>
+</node>
+<node CREATED="1718346721792" ID="ID_779126369" MODIFIED="1718346870694" POSITION="left" TEXT="&#x635;&#x646;&#x62f;&#x648;&#x642;&#x62f;&#x627;&#x631;">
+<font NAME="SansSerif" SIZE="20"/>
+<icon BUILTIN="female2"/>
+<node CREATED="1718347981601" HGAP="26" ID="ID_202150269" MODIFIED="1718348005728" TEXT="&#x648;&#x627;&#x631;&#x62f; &#x6a9;&#x631;&#x62f;&#x646; &#x6a9;&#x62f; &#x645;&#x62d;&#x648;&#x644;&#x627;&#x62a; &#x62e;&#x631;&#x6cc;&#x62f;&#x627;&#x631;&#x6cc; &#x634;&#x62f;&#x647; &#x62a;&#x648;&#x633;&#x637; &#x645;&#x634;&#x62a;&#x631;&#x6cc;" VSHIFT="-46">
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node CREATED="1718348028464" ID="ID_22779840" MODIFIED="1718348032977" TEXT="&#x62a;&#x639;&#x627;&#x645;&#x644; &#x628;&#x627; &#x645;&#x634;&#x62a;&#x631;&#x6cc;">
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+<node CREATED="1718346727745" ID="ID_1202539217" MODIFIED="1718346837438" POSITION="right" TEXT="&#x627;&#x646;&#x628;&#x627;&#x631;&#x62f;&#x627;&#x631;">
+<font NAME="SansSerif" SIZE="20"/>
+<icon BUILTIN="list"/>
+<node CREATED="1718347127689" HGAP="54" ID="ID_1638065180" MODIFIED="1718347148829" TEXT="&#x62e;&#x632;&#x627;&#x646;&#x647; &#x645;&#x62d;&#x635;&#x648;&#x644;&#x627;&#x62a;" VSHIFT="-27">
+<font NAME="SansSerif" SIZE="19"/>
+<node CREATED="1718347150777" HGAP="21" ID="ID_831219483" MODIFIED="1718347326889" TEXT="&#x645;&#x62d;&#x635;&#x648;&#x644;&#x627;&#x62a; &#x645;&#x648;&#x62c;&#x648;&#x62f; &#x62f;&#x631; &#x641;&#x631;&#x648;&#x634;&#x6af;&#x627;&#x647;" VSHIFT="-37">
+<icon BUILTIN="desktop_new"/>
+</node>
+<node CREATED="1718347162785" ID="ID_354856235" MODIFIED="1718347329693" TEXT="&#x645;&#x62d;&#x635;&#x648;&#x644;&#x627;&#x62a; &#x645;&#x648;&#x62c;&#x648;&#x62f; &#x62f;&#x631; &#x627;&#x646;&#x628;&#x627;&#x631;" VSHIFT="20">
+<icon BUILTIN="gohome"/>
+</node>
+</node>
+<node CREATED="1718347304680" ID="ID_449732021" MODIFIED="1718347313193" TEXT="&#x62f;&#x631;&#x6cc;&#x627;&#x641;&#x62a; &#x645;&#x62d;&#x635;&#x648;&#x644;&#x627;&#x62a; &#x648;&#x631;&#x648;&#x62f;&#x6cc;">
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node CREATED="1718347333697" ID="ID_125320304" MODIFIED="1718347347353" TEXT="&#x62f;&#x631;&#x6cc;&#x627;&#x641;&#x62a; &#x627;&#x639;&#x644;&#x627;&#x646;&#x627;&#x62a; &#x627;&#x62a;&#x645;&#x627;&#x645; &#x6a9;&#x627;&#x644;&#x627;">
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+<node CREATED="1718346734025" HGAP="52" ID="ID_347487029" MODIFIED="1718348103543" POSITION="left" TEXT="&#x645;&#x62f;&#x6cc;&#x631;&#x6cc;&#x62a; &#x635;&#x646;&#x62f;&#x648;&#x642; &#x631;&#x641;&#x627;&#x647;" VSHIFT="44">
+<font NAME="SansSerif" SIZE="20"/>
+<icon BUILTIN="folder"/>
+<node CREATED="1718348071352" ID="ID_401533531" MODIFIED="1718348081657" TEXT="&#x62a;&#x639;&#x6cc;&#x6cc;&#x646; &#x646;&#x631;&#x62e; &#x62a;&#x62e;&#x641;&#x6cc;&#x641;">
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node CREATED="1718348084576" ID="ID_1889074952" MODIFIED="1718348107928" TEXT="&#x645;&#x634;&#x627;&#x647;&#x62f;&#x647; &#x633;&#x648;&#x627;&#x628;&#x642; &#x645;&#x634;&#x62a;&#x631;&#x6cc;">
+<font NAME="SansSerif" SIZE="19"/>
+<node CREATED="1718348095136" ID="ID_153874080" MODIFIED="1718348096574" TEXT="&#x62e;&#x631;&#x6cc;&#x62f;"/>
+<node CREATED="1718348096880" ID="ID_1106608313" MODIFIED="1718348100046" TEXT="&#x62e;&#x648;&#x634;&#x200c;&#x62d;&#x633;&#x627;&#x628;&#x6cc;"/>
+</node>
+</node>
+<node CREATED="1718346758529" HGAP="13" ID="ID_963055227" MODIFIED="1718346832845" POSITION="right" TEXT="&#x645;&#x62f;&#x6cc;&#x631;&#x6cc;&#x62a; &#x641;&#x631;&#x648;&#x634;&#x6af;&#x627;&#x647;" VSHIFT="40">
+<font NAME="SansSerif" SIZE="20"/>
+<icon BUILTIN="gohome"/>
+<node CREATED="1718347935625" ID="ID_512670868" MODIFIED="1718347967329" TEXT="&#x645;&#x62f;&#x6cc;&#x631;&#x6cc;&#x62a; &#x6a9;&#x627;&#x631;&#x6a9;&#x646;&#x627;&#x646;">
+<font NAME="SansSerif" SIZE="17"/>
+<node CREATED="1718347944328" ID="ID_80774141" MODIFIED="1718347949102" TEXT="&#x627;&#x639;&#x637;&#x627;&#x6cc; &#x62f;&#x633;&#x62a;&#x631;&#x633;&#x6cc;&#x200c;&#x647;&#x627;&#x6cc; &#x645;&#x646;&#x627;&#x633;&#x628;"/>
+</node>
+</node>
+</node>
+</map>
