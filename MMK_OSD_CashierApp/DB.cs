ec35babdf5ea816f37ec9c7c41edbb7bf0ccc35e
Mehrbod Molla Kazemi <98255802+Mehrbod-MK@users.noreply.github.com>
2024-06-29 00:58:09 +0330

    Added MySQL libraries.

---
+++ MMK_OSD_CashierApp/DB.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MySql.Data;
+
+namespace MMK_OSD_CashierApp
+{
+    public class DB
+    {
+
+    }
+}
