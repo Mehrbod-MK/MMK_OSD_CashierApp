36597aca35585c89dfa8a04bd70d968aea254036
Mehrbod Molla Kazemi <98255802+Mehrbod-MK@users.noreply.github.com>
2024-07-05 15:10:23 +0330

    Added Product model and Dashboard ViewModel.

---
+++ MMK_OSD_CashierApp/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using MMK_OSD_CashierApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.RightsManagement;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMK_OSD_CashierApp.Models
+{
+    public class Product : ViewModelBase
+    {
+        private uint productID;
+        private string productName = string.Empty;
+        private ulong price;
+        private string? vendor = null;
+        private DateTime dateTimeSubmitted;
+        private uint quantity;
+
+        public uint ProductID { get => productID; set => SetProperty(ref productID, value); }
+        public string ProductName { get => productName; set => SetProperty(ref productName, value); }
+        public ulong Price { get => price; set => SetProperty(ref price, value); }
+        public string? Vendor { get => vendor; set => SetProperty(ref vendor, value); }
+        public DateTime DateTimeSubmitted { get => dateTimeSubmitted; set => SetProperty(ref dateTimeSubmitted, value); }
+        public uint Quantity { get => quantity; set => SetProperty(ref quantity, value); }
+    }
+}
---
+++ MMK_OSD_CashierApp/ViewModels/Dashboard_ViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMK_OSD_CashierApp.ViewModels
+{
+    public class Dashboard_ViewModel : ViewModelBase
+    {
+
+    }
+}
