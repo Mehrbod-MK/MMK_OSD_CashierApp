46d44fa5e681bceedb3e68ff318432a6b3cd4ffe
Mehrbod Molla Kazemi <98255802+Mehrbod-MK@users.noreply.github.com>
2024-07-09 02:56:08 +0330

    First login of auto-component addition in InputForm.

--- MMK_OSD_CashierApp/Views/Window_InputForm.xaml.cs
+++ MMK_OSD_CashierApp/Views/Window_InputForm.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MMK_OSD_CashierApp.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +20,45 @@ namespace MMK_OSD_CashierApp.Views
     /// </summary>
     public partial class Window_InputForm : Window
     {
-        public Window_InputForm()
+        public Window_InputForm(InputForm_ViewModel vm_InputForm)
         {
             InitializeComponent();
+
+            this.DataContext = vm_InputForm;
+
+            // Initialize form components.
+            foreach(var field in vm_InputForm.FormFields)
+            {
+                TextBlock txtBlock = new TextBlock()
+                {
+                    FontSize = 17,
+                    FontWeight = FontWeights.DemiBold,
+                    Margin= new Thickness(5.0),
+                };
+                txtBlock.SetBinding(TextBlock.TextProperty, 
+                    new Binding("Question") 
+                    { 
+                        UpdateSourceTrigger = 
+                        UpdateSourceTrigger.PropertyChanged, 
+                        Source = field 
+                    });
+
+                TextBox txtBox = new TextBox()
+                {
+                    FontSize = 18,
+                    TextAlignment = TextAlignment.Center,
+                };
+                txtBox.SetBinding(TextBox.TextProperty, 
+                    new Binding("DefaultResponse")
+                {
+                    UpdateSourceTrigger =
+                    UpdateSourceTrigger.PropertyChanged,
+                    Source = field
+                });
+
+                StackPanel_Fields.Children.Insert(StackPanel_Fields.Children.Count, txtBlock);
+                StackPanel_Fields.Children.Insert(StackPanel_Fields.Children.Count, txtBox);
+            }
         }
     }
 }
